5eb7a5d9e1a9762bad0b6e0b6ad88d71e0e56b09
quocthien18071203 <thiennqse171671@fpt.edu.vn>
2024-05-20 16:55:04 +0700

    update

--- DataAccessLayer/FunewsManagementDbContext.cs
+++ DataAccessLayer/FunewsManagementDbContext.cs
@@ -26,7 +26,7 @@ public partial class FunewsManagementDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-K25C8S6\\SQLEXPRESS;uid=sa;pwd=123456;database=FUNewsManagementDB;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-SE8RIIC;uid=sa;pwd=123456;database=FUNewsManagementDB;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ WPFNewsManagementSystem/StaffManageCategoryInfomation.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFNewsManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for StaffManageCategoryInfomation.xaml
+    /// </summary>
+    public partial class StaffManageCategoryInfomation : Window
+    {
+        public StaffManageCategoryInfomation()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
